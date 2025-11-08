b67d4dcb2074785b3e08c566396cadf066dc7810
adityakumbhar23-arch <aditya.kumbhar23@vit.edu>
2025-11-08 02:57:53 -0500

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,18 @@
-import LoginPanel from "./components/Login/Login"
+
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // ✅ Import your new Register component
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      {/*  Existing login route */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/*  New register route */}
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
